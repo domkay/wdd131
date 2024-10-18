d570238584a86d25e80efb693ce214355ff8427d
domkay <domkayombo@gmail.com>
2024-10-18 22:18:13 +0200

    new

--- scripts/place.js
+++ scripts/place.js
@@ -1,10 +1,23 @@
 function calculateWindChill(temp, windSpeed) {
-    let windChill = 13.12 + 0.6215 * temp - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temp * Math.pow(windSpeed, 0.16));
-    windChill = Math.round(windChill * 10) / 10;
-    return windChill;
+    if ((temp <= 10 && temp >= -50) || (temp <= 50 && temp >= -58)) {
+        if (windSpeed > 4.8 || windSpeed > 3) {
+            let windChill = 13.12 + 0.6215 * temp - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temp * Math.pow(windSpeed, 0.16));
+            windChill = Math.round(windChill * 10) / 10;
+            return windChill;
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
 }
+
 const temperature = 23;
 const windSpeed = 3.5;
 const windChillValue = calculateWindChill(temperature, windSpeed);
 
-document.getElementById("windChill").textContent = windChillValue + "°C";
+if (windChillValue !== null) {
+    document.getElementById("windChill").textContent = windChillValue + "°C";
+} else {
+    document.getElementById("windChill").textContent = "N/A";
+}
